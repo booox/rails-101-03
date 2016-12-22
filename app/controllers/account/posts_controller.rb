3d6713969bbe523609f7a5a8ae48565e9a46284e
booox <booox@users.noreply.github.com>
2016-12-22 23:31:12 +0800

    implement accout/posts edit and delete

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -4,6 +4,7 @@ class Account::PostsController < ApplicationController
     def index
         @posts = current_user.posts
     end
+
 end
 
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_filter :authenticate_user!, :only => [:new, :create]
+    before_filter :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
     # new
     def new
@@ -21,6 +21,30 @@ class PostsController < ApplicationController
         end
     end
 
+    # edit
+    def edit
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+    end
+
+    # update
+    def update
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to account_posts_path, notice: "Post Updated."
+        else
+            render :edit
+        end
+    end
+    
+    # delete
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to account_posts_path, alert: "Post Deleted."
+    end
 
     private
 
